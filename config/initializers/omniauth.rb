9ad6cc69ebb9aaf05c7786162078e7a21562dcf3
John Deeney <JohnCDeeney@gmail.com>
2015-09-22 10:43:45 -0400

    Added simple Facebook login & callback to /auth/facebook

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :facebook, '1633633020219850', 'b6a060a55c41571095331bf16e46e209'
+end
