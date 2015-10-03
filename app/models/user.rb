0485e0814e750ccf8f070e201db38510b9754b17
Nathan Perry <nbp8557@rit.edu>
2015-10-02 21:13:29 -0400

    Added gem to assist with Facebook API calls. Also added the ability to Login and logout and display to the user via the navbar their status (logged in or not).

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ActiveRecord::Base
       user.save
     end
   end
+
+  def getProfile(auth)
+    accessToken = auth['token']
+
+    facebook = Koala::Facebook::API.new(accessToken)
+    userProfile = facebook.get_object("me?fields=name,picture");
+  end
+
+  def getFeed(auth)
+    accessToken = auth['token']
+    facebook = Koala::Facebook::API.new(accessToken)
+    feed = facebook.get_connections("me", "feed")
+
+    return feed;
+  end
 end
