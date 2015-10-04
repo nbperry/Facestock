3cad235df65462677f8734700984635f72e9501e
rnn7726 <rnn7726@rit.edu>
2015-10-04 13:01:45 -0400

    search for stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,27 +6,37 @@ class StocksController < ApplicationController
     yahoo_client = YahooFinance::Client.new
     #this is the stock you will get
     @stock = params[:ticker]
-    chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
-    data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :previous_close, :close, :name], {raw: false})
-    #set everything that you will be using on the page
-    @ask = data.ask
-    @high = data.high
-    @low = data.low
-    @high52 = data.high_52_weeks
-    @low52 = data.low_52_weeks
-    @lastclose = data.previous_close
-    @close = data.close
-    @name = data.name
-    #make the chart 2d array
-    xy = []
-    dates = []
-    values = []
-    chart.each do |this|
-      dates.unshift(this[:trade_date])
-      values.unshift(this[:close].to_f)
+    if( params.has_key?(:ticker))
+      chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
+      data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :close, :name], {raw: false})
+      #set everything that you will be using on the page
+      @ask = data.ask
+      @high = data.high
+      @low = data.low
+      @high52 = data.high_52_weeks
+      @low52 = data.low_52_weeks
+      @close = data.close
+      @name = data.name
+      #make the chart 2d array
+      xy = []
+      dates = []
+      values = []
+      chart.each do |this|
+        dates.unshift(this[:trade_date])
+        values.unshift(this[:close].to_f)
+      end
+    else
+      @ask = 'N/A'
+      @high= 'N/A'
+      @low= 'N/A'
+      @high52 = 'N/A'
+      @low52 = 'N/A'
+      @close = 'N/A'
+      @name = 'Please Enter A Valid Ticker'
+      @stock = 'N/A'
     end
     @show = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(:text => "Stock History For " + @name)
+      f.title(:text => @name)
       f.xAxis(:categories => dates)
       f.series(:name => "Closing Value", :yAxis => 0, :data => values)
       f.yAxis [
@@ -34,7 +44,6 @@ class StocksController < ApplicationController
               ]
       f.chart({:defaultSeriesType=>"line"})
     end
-
   end
   def create
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'stocks/stock'
-  resources :stock
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
