d9153e23881a143e8536c382aae7d920b4af16c5
rnn7726 <rnn7726@rit.edu>
2015-10-04 11:19:47 -0400

    yes charts are working

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'yahoo-finance'
-
+gem 'lazy_high_charts'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,10 +2,13 @@ class StocksController < ApplicationController
   require 'json'
   require 'ostruct'
   def stock
+    #get stocks from yahoo into data, chart info is for the interactive chart
     yahoo_client = YahooFinance::Client.new
+    #this is the stock you will get
     @stock = params[:ticker]
-    @chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*5), end_date: Time::now })
+    chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
     data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :previous_close, :close, :name], {raw: false})
+    #set everything that you will be using on the page
     @ask = data.ask
     @high = data.high
     @low = data.low
@@ -14,6 +17,24 @@ class StocksController < ApplicationController
     @lastclose = data.previous_close
     @close = data.close
     @name = data.name
+    #make the chart 2d array
+    xy = []
+    dates = []
+    values = []
+    chart.each do |this|
+      dates.unshift(this[:trade_date])
+      values.unshift(this[:close].to_f)
+    end
+    @show = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(:text => "Stock History For " + @name)
+      f.xAxis(:categories => dates)
+      f.series(:name => "Closing Value", :yAxis => 0, :data => values)
+      f.yAxis [
+                  {:title => {:text => "Value in Dollars"}}
+              ]
+      f.chart({:defaultSeriesType=>"line"})
+    end
+
   end
   def create
 
