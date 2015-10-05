59b78212444b143541f9bc165736dc17fb54c4cc
Nathan Perry <nbp8557@rit.edu>
2015-10-05 17:28:20 -0400

    Added the ability to run the application in both development and production without having to modifing the Facebook application

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,18 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '1633633020219850', 'b6a060a55c41571095331bf16e46e209',
+  if Rails.env.development?
+
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :facebook, '1634892103427275', '9c7ea987cbbdeaa85012d75d3623feed',
                :scope => 'email,user_birthday,publish_actions', :display => 'popup'
-end
+    end
+
+  else
+
+    Rails.application.config.middleware.use OmniAuth::Builder do
+          provider :facebook, '1633633020219850', 'b6a060a55c41571095331bf16e46e209',
+                   :scope => 'email,user_birthday,publish_actions', :display => 'popup'
+    end
+
+  end
+
