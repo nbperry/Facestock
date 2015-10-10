c0bd1f3abeae67ea1e2652a927340198f90a8bc9
Nathan Perry <nbp8557@rit.edu>
2015-10-09 21:24:54 -0400

    Removed read_stream permission. User's tokens are now updated to prevent runtime issues in the event that a token expires

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
 class User < ActiveRecord::Base
   def self.omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.token = auth.credentials.token
-      user.save
+
+    # If the user already exists update their token info
+    if(self.where(provider: auth.provider, uid: auth.uid).exists?)
+      updatedUser = User.find_by(provider: auth.provider, uid: auth.uid)
+
+      updatedUser.token = auth.credentials.token
+      updatedUser.save
+
+      return updatedUser
+
+    # Otherwise create an entry for this user
+    else
+
+      self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.token = auth.credentials.token
+        user.save
+      end
     end
+
   end
 
   def getProfile(auth)
