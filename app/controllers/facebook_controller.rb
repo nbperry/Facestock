fcb3eb3fcf21072d80c8559136c508da9b9c8e92
nbp8557 <nbp8557@rit.edu>
2015-10-14 08:24:32 -0400

    Removed repeated logic and moved to the homepage controller

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,28 +1,5 @@
 class FacebookController < ApplicationController
 
-
-  def feed
-    #accessToken = env['omniauth.auth']['token']
-    #facebook = Koala::Facebook::API.new(accessToken)
-
-    #@results = params[:page] ? facebook.get_page(params[:page]) : facebook.get_connections("me", "feed")
-
-    #If user is not null then post the message
-    if current_user != nil
-        #if the form message is empty then dont make a post
-
-        accessToken = current_user.token
-        @facebook = Koala::Facebook::API.new(accessToken)
-
-
-        @results = @facebook.get_connections('me', 'feed')
-
-    end
-    #render "facebook/feed"
-    #redirect_to(@results)
-    #redirect_to root_url
-  end
-
   #method for sharing a status on facebook
   def status
 
