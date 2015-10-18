9227a9df06abc331e78da132b9f8e2c5c0f5e440
Laura Durant <lkd8070@rit.edu>
2015-10-18 11:08:20 -0400

    seeding for stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Stock.delete_all
+yahoo_client = YahooFinance::Client.new
+symbols = yahoo_client.symbols_by_market('us', 'nyse')
+data = yahoo_client.quotes(symbols, [:symbol, :name, :ask, :change])
+date = Time.new
+lastItemName = ''
+for item in data
+  if item.name == 'N/A'
+    item.name = lastItemName
+  end
+  ticker = Stock.create("ticker": item.symbol, "name": item.name, "current_price": item.ask, "change": item.change, "updated": date)
+  lastItemName = item.name
+end
