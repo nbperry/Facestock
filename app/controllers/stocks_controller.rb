8f0c3a08a02832ed52c1253e3989408d99d2c410
Laura Durant <lkd8070@rit.edu>
2015-10-18 11:09:02 -0400

    seeding for stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -52,14 +52,11 @@ class StocksController < ApplicationController
 
   end
   def index
-    yahoo_client = YahooFinance::Client.new
-    @symbols = yahoo_client.symbols_by_market('us', 'nyse')
-    @data = yahoo_client.quotes(@symbols, [:symbol, :name, :ask, :change])
-    @stocks = Array.new
-    date = '10/04/2015'
-    for item in @data
-      ticker = Stock.new(ticker: item.symbol, name: item.name, current_price: item.ask, change: item.change, updated: date)
-      @stocks << ticker
-    end
+    #for now we'll get them here
+    @stocks = Stock.all
+  end
+  def data
+    #return data from the database for ajax calls
+    @stocks = Stock.all
   end
 end
