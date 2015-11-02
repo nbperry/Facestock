2a5644455954abe9ce99feb7e3895cc610fc7759
rnn7726 <rnn7726@rit.edu>
2015-11-01 19:07:56 -0500

    idk who did this but it's not working properly, just put it in a new seed file and fix it there for ease of reading

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 load 'db/seeds/userstocks.rb'
 
-Stock.delete_all
-yahoo_client = YahooFinance::Client.new
-symbols = yahoo_client.symbols_by_market('us', 'nyse')
-data = yahoo_client.quotes(symbols, [:symbol, :name, :ask, :change])
-date = Time.new
-lastItemName = ''
-for item in data
-  if item.name == 'N/A'
-    item.name = lastItemName
-  end
-  ticker = Stock.create("ticker": item.symbol, "name": item.name, "current_price": item.ask, "change": item.change, "updated": date)
-  lastItemName = item.name
-end
+#Stock.delete_all
+#yahoo_client = YahooFinance::Client.new
+#symbols = yahoo_client.symbols_by_market('us', 'nyse')
+#data = yahoo_client.quotes(symbols, [:symbol, :name, :ask, :change])
+#date = Time.new
+#lastItemName = ''
+#for item in data
+#  if item.name == 'N/A'
+#    item.name = lastItemName
+#  end
+#  ticker = Stock.create("ticker": item.symbol, "name": item.name, "current_price": item.ask, "change": item.change, "updated": date)
+#  lastItemName = item.name
+#end
