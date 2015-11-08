263517aada3a8edc10d86953adcc8f8f62cae8c4
rnn7726 <rnn7726@rit.edu>
2015-11-08 18:14:44 -0500

    add scaling chart

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,7 +9,7 @@ class StocksController < ApplicationController
     @_current_user ||= session[:user_id] &&
         User.find_by(id: session[:user_id])
     if( params.has_key?(:ticker))
-      chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
+      chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*365), end_date: Time::now })
       data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :close, :name], {raw: false})
       #set everything that you will be using on the page
       @ask = data.ask
@@ -22,7 +22,7 @@ class StocksController < ApplicationController
       dates = []
       values = []
       chart.each do |this|
-        dates.unshift(this[:trade_date])
+        dates.unshift(this[:trade_date].to_date)
         values.unshift(this[:close].to_f)
       end
     else
@@ -37,12 +37,33 @@ class StocksController < ApplicationController
     end
     @show = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => @name)
-      f.xAxis(:categories => dates)
+      f.xAxis(:categories => dates, :type => 'datetime')
       f.series(:name => "Closing Value", :yAxis => 0, :data => values)
       f.yAxis [
                   {:title => {:text => "Value in Dollars"}}
               ]
-      f.chart({:defaultSeriesType=>"line"})
+      f.chart({:defaultSeriesType=>"line", :zoomType => "x"})
+      f.plotOptions({
+          area: {
+              fillColor: {
+                  linearGradient: {
+                      x1: 0,
+                      y1: 0,
+                      x2: 0,
+                      y2: 1
+                  }
+              },
+              marker: {
+                  radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                  hover: {
+                      lineWidth: 1
+                  }
+              }
+          }
+      })
     end
   end
   def create
