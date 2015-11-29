2db6638da0ca36e56a707b89bcf4bb2a5168699e
rnn7726 <rnn7726@rit.edu>
2015-11-29 16:23:05 -0500

    transaction button works properly, now need to add the ability for transactions

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,77 +2,81 @@ class StocksController < ApplicationController
   require 'json'
   require 'ostruct'
   def stock
-    begin
-      #get stocks from yahoo into data, chart info is for the interactive chart
-      yahoo_client = YahooFinance::Client.new
-      #this is the stock you will get
-      @stock = params[:ticker]
-      @_current_user ||= session[:user_id] &&
-          User.find_by(id: session[:user_id])
-      if( params.has_key?(:ticker))
-        chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
-        data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :close, :name], {raw: false})
-        #set everything that you will be using on the page
-        @ask = data.ask
-        @high = data.high
-        @low = data.low
-        @high52 = data.high_52_weeks
-        @low52 = data.low_52_weeks
-        @close = data.close
-        @name = data.name
-        dates = []
-        values = []
-        chart.each do |this|
-          dates.unshift(this[:trade_date].to_date)
-          values.unshift(this[:close].to_f)
+    if params[:transaction]
+      # Redirect to show the newly created record, for example.
+      flash[:notice] = "Stock Transaction Went Through."
+      redirect_to :back, :alert => "Transaction Happened!"
+    else
+      # Redirect to new form, for example.
+      begin
+        #get stocks from yahoo into data, chart info is for the interactive chart
+        yahoo_client = YahooFinance::Client.new
+        #this is the stock you will get
+        @stock = params[:ticker]
+        @_current_user ||= session[:user_id] &&
+            User.find_by(id: session[:user_id])
+        if( params.has_key?(:ticker))
+          chart = yahoo_client.historical_quotes(@stock, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
+          data = yahoo_client.quote([@stock], [:ask, :high, :low, :high_52_weeks, :low_52_weeks, :close, :name], {raw: false})
+          #set everything that you will be using on the page
+          @ask = data.ask
+          @high = data.high
+          @low = data.low
+          @high52 = data.high_52_weeks
+          @low52 = data.low_52_weeks
+          @close = data.close
+          @name = data.name
+          dates = []
+          values = []
+          chart.each do |this|
+            dates.unshift(this[:trade_date].to_date)
+            values.unshift(this[:close].to_f)
+          end
+        else
+          @ask = 'N/A'
+          @high= 'N/A'
+          @low= 'N/A'
+          @high52 = 'N/A'
+          @low52 = 'N/A'
+          @close = 'N/A'
+          @name = 'Please Enter A Valid Ticker'
+          @stock = 'N/A'
         end
-      else
-        @ask = 'N/A'
-        @high= 'N/A'
-        @low= 'N/A'
-        @high52 = 'N/A'
-        @low52 = 'N/A'
-        @close = 'N/A'
-        @name = 'Please Enter A Valid Ticker'
-        @stock = 'N/A'
+        @show = LazyHighCharts::HighChart.new('graph') do |f|
+          f.title(:text => @name)
+          f.xAxis(:categories => dates, :type => 'datetime')
+          f.series(:name => "Closing Value", :yAxis => 0, :data => values)
+          f.yAxis [
+                      {:title => {:text => "Value in Dollars"}}
+                  ]
+          f.chart({:defaultSeriesType=>"line", :zoomType => "x"})
+          f.plotOptions({
+                            area: {
+                                fillColor: {
+                                    linearGradient: {
+                                        x1: 0,
+                                        y1: 0,
+                                        x2: 0,
+                                        y2: 1
+                                    }
+                                },
+                                marker: {
+                                    radius: 2
+                                },
+                                lineWidth: 1,
+                                states: {
+                                    hover: {
+                                        lineWidth: 1
+                                    }
+                                }
+                            }
+                        })
+        end
+      rescue OpenURI::HTTPError => ex
+        flash[:alert] = "That is not a valid ticker symbol!"
+        redirect_to :back, :alert => "That is not a valid ticker symbol!"
       end
-      @show = LazyHighCharts::HighChart.new('graph') do |f|
-        f.title(:text => @name)
-        f.xAxis(:categories => dates, :type => 'datetime')
-        f.series(:name => "Closing Value", :yAxis => 0, :data => values)
-        f.yAxis [
-                    {:title => {:text => "Value in Dollars"}}
-                ]
-        f.chart({:defaultSeriesType=>"line", :zoomType => "x"})
-        f.plotOptions({
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    }
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                }
-            }
-        })
-      end
-    rescue OpenURI::HTTPError => ex
-      flash[:alert] = "That is not a valid ticker symbol!"
-      redirect_to :back, :alert => "That is not a valid ticker symbol!"
     end
-  end
-  def stock_transaction
-    flash[:alert] = "Stock Transaction Went Through."
   end
   def create
 
